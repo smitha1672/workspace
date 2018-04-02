78b7f7e7c6b86f0da64e04a9eb6e523da823071b
smith.hu <smith.hu@cyweemotion.com>
2018-04-02 14:40:10 +0800

    [add] 8. String to Integer (atoi)
    Note:
    https://leetcode.com/problems/string-to-integer-atoi/description/

---
+++ cSkillTraning/leetcode/_8_StringtoInteger_atoi/main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <limits.h>
+#define LEETCODE_LINK "https://leetcode.com/problems/string-to-integer-atoi/description/\n"
+/**
+Implement atoi to convert a string to an integer.
+
+Hint: Carefully consider all possible input cases. If you want a challenge, please do not see below and ask yourself what are the possible input cases.
+Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
+
+Requirements for atoi:
+
+The function first discards as many whitespace characters as necessary until the first non-whitespace character is found. Then, starting from this character, takes an optional initial plus or minus sign followed by as many numerical digits as possible, and interprets them as a numerical value.
+
+The string can contain additional characters after those that form the integral number, which are ignored and have no effect on the behavior of this function.
+
+If the first sequence of non-whitespace characters in str is not a valid integral number, or if no such sequence exists because either str is empty or it contains only whitespace characters, no conversion is performed.
+
+If no valid conversion could be performed, a zero value is returned. If the correct value is out of the range of representable values, INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
+
+gcc -o main main.c -lm
+
+int myAtoi(char* str) {
+}
+
+a next challenge:
+*/
+
+int myAtoi(char* str)
+{
+  int ret = 0;
+  long long val = 0;
+
+  for (i = 0; str[i] != '\0', i++)
+    val = val*10+str[i]-'0';
+
+  if (val >= INT_MIN || val <= INT_MAX)
+    ret = (int)val;
+}
+
+void main (void)
+{
+  char input[12] = "3";
+  int output = 0;
+
+  printf(LEETCODE_LINK);
+  printf("input: %s\n", input);
+  output = myAtoi(input);
+  printf("out: %d\n", output);
+
+}

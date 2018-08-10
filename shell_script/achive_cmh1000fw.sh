e67756efa226d6e0b19b86e73aeebbbe80be263c
smith.hu <smith.hu@cyweemotion.com>
2018-08-09 21:08:22 -0700

    [change] get version from makefile of variant

--- shell_script/achive_cmh1000fw.sh
+++ shell_script/achive_cmh1000fw.sh
@@ -1,20 +1,20 @@
 # ! /bin/bash
-GIT_TAG=$1
+VER_NUM=$(echo "v15.$(cat chre/firmware/misc/variant/$1/Makefile | grep VARIANT | sed 's/FLAGS += -DVARIANT_VER=0x//')")
+VARIANT_MK_PATH="chre/firmware/misc/variant/$1/Makefile"
 
 achive_fw_image()
 {
-  GIT_TAG_NUMBER=$(git tag -l --sort=-taggerdate | grep $GIT_TAG | head -1)
-  if [ -z "$GIT_TAG_NUMBER" ]; then
-    echo "GIT TAG: $1 is not existing"
-  else
-    rm -f sensorhub_fw_$GIT_TAG_NUMBER.zip
-    echo -e "Create sensorhub_fw_$GIT_TAG_NUMBER.zip\n"
-    find ./chre/firmware -type f -name "os.checked.*" -print | zip -qj sensorhub_fw_$GIT_TAG_NUMBER.zip -@
-  fi
+  rm -v sensorhub_fw_"${VER_NUM}".zip
+  echo -e "Create sensorhub_fw_"${VER_NUM}".zip\n"
+  find ./chre/firmware -type f -name "os.checked.*" -print | zip -qj sensorhub_fw_"${VER_NUM}".zip -@
 }
 
-if [ -z "$GIT_TAG" ]; then
-  echo "ERROR: \$1 is empty, you have to fill GIT tag keyword is similar \"04B\", \"08D\"...etc"
-else
+if [ -f $VARIANT_MK_PATH ]; then
+  echo "Project name is: $1"
   achive_fw_image
+else
+  echo "Error! Yours project name is empty: $1"
+  echo "You should fill that it like as \$ source achive_cmh1000fw.sh 07_w906_zwcm0118"
+  echo "Project name is as below:"
+  find . -path "*/misc/variant/*" -type d -print | sed 's/.\/chre\/firmware\/misc\/variant\///'
 fi

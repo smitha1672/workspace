7633179516926bd267d14e09ffb68cb183cc4685
smith.hu <smith.hu@cyweemotion.com>
2019-05-22 20:05:28 +0800

    [add] unordered_map and vector c++11 example code

--- cSkillTraning/leetcode/_981_Time_Based_Key-Value_Store/main.cpp
+++ cSkillTraning/leetcode/_981_Time_Based_Key-Value_Store/main.cpp
@@ -122,12 +122,81 @@ TimeMap::get(std::string key, int timestamp)
     return prev(it)->second;
 }
 
+static void
+_vector_example1_cpp11(void)
+{
+    using namespace std;
+    cout << "_vector_example1_cpp11 {" << endl;
+    vector<int>a = {1, 2, 3, 4, 5};
+    for (int i = 0; i < a.size(); i++)
+        cout << a[i] << endl;
+    cout << "}" << endl;
+}
+
+static void
+_unordered_map_example2(void)
+{
+    using namespace std;
+    // Declaring umap to be of <string, double> type
+    // key will be of string type and mapped value will
+    // be of double type
+    unordered_map<string, double> umap;
+    // inserting values by using [] operator
+    umap["PI"] = 3.14;
+    umap["root2"] = 1.414;
+    umap["root3"] = 1.732;
+    umap["log10"] = 2.302;
+    umap["loge"] = 1.0;
+    // inserting value by insert function
+    umap.insert(make_pair("e", 2.718));
+    string key = "PI";
+    // If key not found in map iterator to end is returned
+    if (umap.find(key) == umap.end())
+        cout << key << " not found\n\n";
+    // If key found then iterator to that key is returned
+    else
+        cout << "Found " << key << "\n\n";
+    key = "lambda";
+    if (umap.find(key) == umap.end())
+        cout << key << " not found\n";
+    else
+        cout << "Found " << key << endl;
+    // iterating over all value of umap
+    unordered_map<string, double>:: iterator itr;
+    cout << "\nAll Elements : \n";
+    for (itr = umap.begin(); itr != umap.end(); itr++) {
+        // itr works as a pointer to pair<string, double>
+        // type itr->first stores the key part and
+        // itr->second stroes the value part
+        cout << itr->first << " " << itr->second << endl;
+    }
+}
+
+static void
+_unordered_map_example1(void)
+{
+    using namespace std;
+    cout << "_unordered_map_example1 {" << endl;
+    // Declaring umap to be of <string, int> type
+    // key will be of string type and mapped value will
+    // be of double type
+    unordered_map<string, int> umap;
+    // inserting values by using [] operator
+    umap["GeeksforGeeks"] = 10;
+    umap["Practice"] = 20;
+    umap["Contribute"] = 30;
+    // Traversing an unordered map
+    for (auto x : umap)
+        cout << x.first << " " << x.second << endl;
+    cout << "}" << endl;
+}
+
+
 int
 main(void)
 {
-    std::vector<int>a = {1, 2, 3, 4, 5};
-    for (int i = 0; i < a.size(); i++)
-        std::cout << a[i] << std::endl;
+    _vector_example1_cpp11();
+    _unordered_map_example1();
     TimeMap kv;
     kv.set("foo", "bar", 1);
     std::cout << kv.get("foo", 1) << std::endl;
